bef4a7e1da21ae4359825da64262081907e128f4
Nimbus Deploy <nimbus-deploy@nimbus.co>
2026-05-26 20:31:16 +0000

    3.0.0-rc.2 Release

--- Package.swift
+++ Package.swift
@@ -5,33 +5,18 @@ import PackageDescription
 let package = Package(
     name: "NimbusKit",
     platforms: [.iOS(.v13)],
-    targets: [
-        .binaryTarget(
-            name: "OMSDK-Adsbynimbus",
-            url: "https://adsbynimbus-public.s3.us-east-1.amazonaws.com/iOS/external/omsdk/1.6.5/OMSDK_Adsbynimbus-1.6.5.zip",
-            checksum: "0948cf0e1d68d53904d7f8c96703457496adc1b0d8f10782d59409b0f7a6e974"
-        )
-    ]
 )
 
 package.products = [
     .library(
         name: "NimbusKit",
-        targets: ["NimbusTarget"]
-    )
-]
-
-package.targets += [
-    .target(
-        name: "NimbusTarget",
-        dependencies: ["OMSDK-Adsbynimbus", "NimbusKit"],
-        path: "Sources/NimbusKit"
+        targets: ["NimbusKit"]
     )
 ]
 package.targets += [
   .binaryTarget(
     name: "NimbusKit",
-    url: "https://adsbynimbus-public.s3.amazonaws.com/iOS/sdks/3.0.0-rc.1/NimbusKit-3.0.0-rc.1.zip",
-    checksum: "ba81d3bdb2cf670fa8107822337889ca8f7cc3e734c68a6484ce80587e2cf06b"
+    url: "https://adsbynimbus-public.s3.amazonaws.com/iOS/sdks/3.0.0-rc.2/NimbusKit-3.0.0-rc.2.zip",
+    checksum: "5d45cbab3be9d62e7ca76e9359319904d1c7e4898b3d0e11ed7a2089869e6c4a"
   ),
 ]
--- Sources/NimbusKit/Export.swift
+++
@@ -1,7 +0,0 @@
-//
-//  Export.swift
-//
-//  DO NOT REMOVE THIS FILE, it is used with Swift Packager Manager to fix Dependency Resolution
-//
-//  Created on 3/14/23.
-//
